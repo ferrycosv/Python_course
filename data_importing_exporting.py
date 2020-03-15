b1c0abebc8273273c2cc497f45f634c04141f5f9
Francisco Fernández <ferrycosv@gmail.com>
2020-03-15 18:08:09 +0100

    Data importing/exporting basics

---
+++ data_importing_exporting.py
@@ -0,0 +1,15 @@
+# Basic importing of data Python script
+import pandas as pd
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
+df = pd.read_csv(url, header=None)
+headers = ["symboling", "normalized-losses", "make", "fuel-type", "aspiration", "num-of-doors", "body-style",
+           "drive-wheels", "engine-location", "wheel-base", "length", "width", "height", "curb-weight", "engine-type",
+           "num-of-cylinders", "engine-size", "fuel-system", "bore", "stroke", "compression-ratio", "horse power",
+           "peak-rpm", "city-mpg", "highway-mpg", "price"]
+df.columns = headers
+print("First 10 records \n")
+print(df.head(10))
+print("Last 10 records \n")
+print(df.tail(10))
+df.to_csv("/Users/ferrycosv/PycharmProjects/Python_course/imports-85.csv")
